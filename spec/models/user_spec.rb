4bb98f5cc740e83ba1e78c2fc5e8b9ead1a4218d
Nathan Brown <nathan.brown263@gmail.com>
2021-11-30 16:15:25 -0700

    fix: Add test for User validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it {should have_many :user_viewing_parties}
     it {should have_many(:viewing_parties).through(:user_viewing_parties)}
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
   end
 end
