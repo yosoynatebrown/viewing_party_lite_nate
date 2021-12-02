adee066caa500a577d68b686a8b813199fa1f55c
Billy Frey <bfrey16@gmail.com>
2021-12-02 11:06:20 -0800

    feat: add services for movie credits and additional movie details

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -13,6 +13,18 @@ class TmdbService
     data = JSON.parse(response.body, symbolize_names: true)
   end
 
+  def movie_details(movie_id)
+    conn = Faraday.new(url: "https://api.themoviedb.org/3")
+    response = conn.get("/movie/#{movie_id}?api_key=#{ENV['tmdb_key']}&language=en-US")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def movie_credits(movie_id)
+    conn = Faraday.new(url: "https://api.themoviedb.org/3")
+    response = conn.get("/movie/#{movie_id}/credits?api_key=#{ENV['tmdb_key']}&language=en-US")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
   # def reviews
   #   response = get_url('https://api.themoviedb.org/3').get("little-esty-shop/contributors")
   #   JSON.parse(response.body, symbolize_names: true)
