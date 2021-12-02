42f4d9bc0765518b4a498a6e464c1af5d1a0e09e
Billy Frey <bfrey16@gmail.com>
2021-12-02 15:59:26 -0800

    feat: add facade for movie details that combines cast and movie details into a movie object

--- app/facades/tmdb_facade.rb
+++ app/facades/tmdb_facade.rb
@@ -8,6 +8,16 @@ class TmdbFacade
     end
   end
 
+  def self.movie_details(id)
+    Rails.cache.fetch('movie_details', :expires => 4.hour) do
+      json = service.movie_details(id)
+      movie = Movie.new(json)
+      movie.add_details(json)
+      json = service.movie_credits(id)
+      movie.add_cast(json)
+      movie
+  end
+
   def self.create_search_movies(search)
     Rails.cache.fetch('review', :expires => 4.hour) do
       json = service.search_movies(search)
