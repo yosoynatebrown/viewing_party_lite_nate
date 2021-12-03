ab3c09ad3c06cdb62de90bc83ae5ed5e2d2b3527
Nathan Brown <nathan.brown263@gmail.com>
2021-12-03 12:45:50 -0700

    test: SimpleCov to 100%

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,6 +1,6 @@
 class TmdbService
   def self.top_rated_movies
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    conn = get_url(url: "https://api.themoviedb.org")
 
     response = conn.get("/3/movie/top_rated?api_key=#{ENV['tmdb_key']}&language=en-US&page=1")
     json = JSON.parse(response.body, symbolize_names: true)
@@ -8,7 +8,7 @@ class TmdbService
   end
 
   def self.search_movies(search)
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    conn = get_url(url: "https://api.themoviedb.org")
 
     response = conn.get("/3/search/movie?api_key=#{ENV['tmdb_key']}&language=en-US&query=#{search}&page=1&include_adult=false")
     json = JSON.parse(response.body, symbolize_names: true)
@@ -16,14 +16,14 @@ class TmdbService
   end
 
   def self.movie_details(movie_id)
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    conn = get_url(url: "https://api.themoviedb.org")
     response = conn.get("/3/movie/#{movie_id}?api_key=#{ENV['tmdb_key']}&language=en-US")
 
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.movie_credits(movie_id)
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    conn = get_url(url: "https://api.themoviedb.org")
     response = conn.get("/3/movie/#{movie_id}/credits?api_key=#{ENV['tmdb_key']}&language=en-US")
 
     JSON.parse(response.body, symbolize_names: true)
@@ -31,13 +31,13 @@ class TmdbService
 
 
   def self.reviews(movie_id)
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    conn = get_url(url: "https://api.themoviedb.org")
     response = conn.get("/3/movie/#{movie_id}/reviews?api_key=#{ENV['tmdb_key']}&language=en-US")
 
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_url(url)
+  def self.get_url(url)
     Faraday.new(url)
   end
 
