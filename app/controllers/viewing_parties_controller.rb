927d6b44e1279098beb991166783d507c808d916
Nathan Brown <nathan.brown263@gmail.com>
2021-12-03 10:57:19 -0700

    fix: Add validation for length of viewing party

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -9,8 +9,10 @@ class ViewingPartiesController < ApplicationController
     viewing_party = ViewingParty.new(party_params)
     user = User.find(params[:host_id])
     user_ids = params[:users]
+
+    movie = TmdbFacade.movie_details(viewing_party.movie_id)
     
-    if viewing_party.save
+    if viewing_party.length >= movie.length && viewing_party.save
       viewing_party.users << user
       user_ids.each do |id|
         invitee = User.find(id.to_i)
@@ -20,7 +22,7 @@ class ViewingPartiesController < ApplicationController
       redirect_to "/users/#{user.id}/"
     else
       redirect_to "/users/#{params[:user_id]}/movies/#{params[:movie_id]}/viewing_parties/new"
-      flash[:alert] = "Error: #{error_message(user.errors)}"
+      flash[:alert] = "Error: Something went wrong. Check that your party duration is not shorter than your movie runtime"
     end
   end
 
